d15958d6afa0a2abcd793708085597431c682c70
Chetanya Kandhari <chetanya.kandhari@brightscout.com>
2019-01-17 22:53:22 +0530

    MI-270: Base64 encode LTI request and set it in a cookie

--- web/lti.go
+++ web/lti.go
@@ -1,11 +1,19 @@
 package web
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"net/http"
+	"net/url"
+	"time"
 
 	"github.com/mattermost/mattermost-server/mlog"
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/utils"
+)
+
+const (
+	LTI_DATA_COOKIE = "MMLTIDATA"
 )
 
 func (w *Web) InitLti() {
@@ -19,7 +27,18 @@ func loginWithLti(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Validate request
+	valid := isRequestValid(c, r)
+	if !valid {
+		c.Err = model.NewAppError("loginWithLti", "api.lti.login.app_error", nil, "", http.StatusNotImplemented)
+		return
+	}
+
+	setLTIDataCookie(c, w, r)
+
+	http.Redirect(w, r, c.GetSiteURLHeader()+"/signup_lti", http.StatusFound)
+}
+
+func isRequestValid(c *Context, r *http.Request) bool {
 	lmss := c.App.Config().LTISettings.GetKnownLMSs()
 	ltiConsumerKey := r.FormValue("oauth_consumer_key")
 	var ltiConsumerSecret string
@@ -36,17 +55,49 @@ func loginWithLti(c *Context, w http.ResponseWriter, r *http.Request) {
 
 	if ltiConsumerSecret == "" {
 		mlog.Error("Consumer secret not found for consumer key: " + ltiConsumerKey)
-		c.Err = model.NewAppError("loginWithLti", "api.lti.login.app_error", nil, "", http.StatusNotImplemented)
-		return
+		return false
 	}
 
 	p := utils.NewProvider(ltiConsumerSecret, c.GetSiteURLHeader()+c.Path)
 	p.ConsumerKey = ltiConsumerKey
 	if ok, err := p.IsValid(r); err != nil || ok == false {
 		mlog.Error("Invalid LTI request: " + err.Error())
-		c.Err = model.NewAppError("loginWithLti", "api.lti.login.app_error", nil, "", http.StatusNotImplemented)
-		return
+		return false
 	}
 
-	http.Redirect(w, r, c.GetSiteURLHeader()+"/signup_lti", http.StatusFound)
+	return true
+}
+
+func encodeLTIRequest(v url.Values) string {
+	if v == nil {
+		return ""
+	}
+	form := make(map[string]string)
+	for key, value := range v {
+		form[key] = value[0]
+	}
+	res, err := json.Marshal(form)
+	if err != nil {
+		mlog.Error("Error in json.Marshal: " + err.Error())
+		return ""
+	}
+
+	return base64.StdEncoding.EncodeToString([]byte(string(res)))
+}
+
+func setLTIDataCookie(c *Context, w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // to populate r.Form
+	maxAge := 600 // 10 minutes
+	expiresAt := time.Unix(model.GetMillis()/1000+int64(maxAge), 0)
+	cookie := &http.Cookie{
+		Name:     LTI_DATA_COOKIE,
+		Value:    encodeLTIRequest(r.Form),
+		Path:     "/",
+		MaxAge:   maxAge,
+		Expires:  expiresAt,
+		Domain:   c.App.GetCookieDomain(),
+		HttpOnly: false,
+	}
+
+	http.SetCookie(w, cookie)
 }
